3e9bb727f19fdbba036e5fbd22d70b3aac138f0d
ManunuGM <mgomezmartinez08@gmail.com>
2025-12-03 13:44:40 +0100

    Update sesiones.php

--- sesiones.php
+++ sesiones.php
@@ -1,4 +1,21 @@
 <?php
-        
-    
-?>
+$usuario_valido = "madrid1@empresa.com";
+$password_valido = "1234";
+
+$usuario = $_POST['usuario'] ?? '';
+$password = $_POST['password'] ?? '';
+$recordarme = isset($_POST['recordarme']);
+
+if ($usuario === $usuario_valido && $password === $password_valido) {
+    if ($recordarme) {
+        setcookie("usuario", $usuario, time() + (86400 * 7), "/"); 
+    } else {
+        setcookie("usuario", $usuario, 0, "/"); 
+    }
+
+    header("Location: categorias.php");
+    exit();
+} else {
+    header("Location: login.php");
+    exit();
+}
